665227616198cc7c9fd23e4ac1487e84c9c7f255
newtimestube <newtimestube@gmail.com>
2018-11-13 23:13:19 +0100

    dinamic selects

---
+++ JSON/act1/tipoViviendas.php
@@ -0,0 +1,12 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+require_once ("../conn.php");
+
+$statement = "SELECT tipo_vivienda.id, tipo_vivienda_has_idioma.nombre 
+from tipo_vivienda, tipo_vivienda_has_idioma 
+where tipo_vivienda_has_idioma.idTipo_vivienda = tipo_vivienda.id";
+
+$res = $conn->prepare($statement);
+$res->execute();
+$rows = $res->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($rows);
